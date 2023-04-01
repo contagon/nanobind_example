c01c70768658310def3513470e943d5344a4ae92
Easton Potokar <contagon6@gmail.com>
2023-04-01 09:24:44 -0400

    Added eigen example

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,9 +1,16 @@
+#include <Eigen/Core>
+
 #include <nanobind/nanobind.h>
+#include <nanobind/eigen/dense.h>
 
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
+Eigen::Matrix<double,1,1> add(Eigen::Matrix<double,1,1> a, Eigen::Matrix<double,1,1> b=Eigen::Matrix<double,1,1>::Zero(3)){
+    return a + b;
+}
+
 NB_MODULE(nanobind_example_ext, m) {
-    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    m.def("add", &add, "a"_a, "b"_a=Eigen::Matrix<double,1,1>::Zero());
 }
